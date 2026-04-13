0f16f0662fe7ceb3bef0e5d33015fda1d2d3f0eb
Neo Rafajlovic <97417002+NeoRafajlovic@users.noreply.github.com>
2026-04-13 15:14:38 -0400

    Update shrek.bat

--- shrek.bat
+++ shrek.bat
@@ -31,6 +31,7 @@ set "file=%temp%\shrek_popup.vbs"
 >>"%file%" echo Loop
 
 curl -L "https://i.pinimg.com/736x/75/6b/bb/756bbb4328e246e8608500ace5d9af0d.jpg" -o "%temp%\shrek.jpg"
+timeout /t 3 /nobreak >nul
 start "" /max mspaint "%temp%\shrek.jpg"
 
 timeout /t 1 /nobreak >nul
